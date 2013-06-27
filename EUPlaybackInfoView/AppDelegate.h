f248fe1512aeee40c895cbff52d7a59ffa4d42a5
Esteban Uribe <estebanuribe@gmail.com>
2013-06-27 13:58:18 -0700

    Initial Commit

---
+++ EUPlaybackInfoView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  EUPlaybackInfoView
+//
+//  Created by Esteban Uribe on 6/27/13.
+//  Copyright (c) 2013 Esteban Uribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ EUPlaybackInfoView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EUPlaybackInfoView
+//
+//  Created by Esteban Uribe on 6/27/13.
+//  Copyright (c) 2013 Esteban Uribe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
