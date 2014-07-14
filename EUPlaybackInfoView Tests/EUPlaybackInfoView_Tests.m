fb073c494a8c3fb457b40699a25165afdc8cf453
Esteban Uribe <estebanuribe@tinderopss-MacBook-Pro.local>
2014-07-14 15:51:24 -0700

    adding tests

---
+++ EUPlaybackInfoView Tests/EUPlaybackInfoView_Tests.m
@@ -0,0 +1,34 @@
+//
+//  EUPlaybackInfoView_Tests.m
+//  EUPlaybackInfoView Tests
+//
+//  Created by Esteban Uribe on 7/14/14.
+//  Copyright (c) 2014 Esteban Uribe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EUPlaybackInfoView_Tests : XCTestCase
+
+@end
+
+@implementation EUPlaybackInfoView_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
